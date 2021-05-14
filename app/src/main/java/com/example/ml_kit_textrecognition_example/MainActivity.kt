1ce2ff2d9c1140ba6d833e2df752320fe58c12c0
tobikuch <kuch@hm.edu>
2021-05-14 13:22:52 +0200

    first

--- app/src/main/java/com/example/ml_kit_textrecognition_example/MainActivity.kt
+++ app/src/main/java/com/example/ml_kit_textrecognition_example/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.ml_kit_textrecognition_example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ml_kit_textrecognition_example.ui.main.MainFragment
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Timber.plant(Timber.DebugTree())
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
--- app/src/main/java/com/example/ml_kit_textrecognition_example/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/ml_kit_textrecognition_example/ui/main/MainFragment.kt
@@ -1,12 +1,21 @@
 package com.example.ml_kit_textrecognition_example.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.app.Activity.RESULT_OK
+import android.content.ComponentName
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.ml_kit_textrecognition_example.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.ml_kit_textrecognition_example.databinding.MainFragmentBinding
+import java.io.File
+
 
 class MainFragment : Fragment() {
 
@@ -14,17 +23,92 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding: MainFragmentBinding
+
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFragmentBinding.inflate(inflater)
+
+        binding.analyzeImageButton.setOnClickListener{ clicked() }
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun clicked(){
+        openImageIntent()
+    }
+
+    private var outputFileUri: Uri? = null
+
+    private fun openImageIntent() {
+
+        // Determine Uri of camera image to save.
+        val root =
+            File(Environment.getExternalStorageState() + File.separator.toString() + "MyDir" + File.separator)
+        root.mkdirs()
+        val fname: String = System.currentTimeMillis().toString() + ".jpg"
+        val sdImageMainDirectory = File(root, fname)
+        outputFileUri = Uri.fromFile(sdImageMainDirectory)
+
+        // Camera.
+        val cameraIntents: MutableList<Intent> = ArrayList()
+        val captureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val packageManager: PackageManager = context!!.packageManager
+        val listCam = packageManager.queryIntentActivities(captureIntent, 0)
+        for (res in listCam) {
+            val packageName = res.activityInfo.packageName
+            val intent = Intent(captureIntent)
+            intent.component = ComponentName(packageName, res.activityInfo.name)
+            intent.setPackage(packageName)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
+            cameraIntents.add(intent)
+        }
+
+        // Filesystem.
+        val galleryIntent = Intent()
+        galleryIntent.type = "image/*"
+        galleryIntent.action = Intent.ACTION_GET_CONTENT
+
+        // Chooser of filesystem options.
+        val chooserIntent = Intent.createChooser(galleryIntent, "Select Source")
+
+        // Add the camera options.
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toTypedArray())
+        startActivityForResult(chooserIntent, 200)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 200) {
+                val isCamera: Boolean
+                isCamera = if (data == null) {
+                    true
+                } else {
+                    val action = data.action
+                    if (action == null) {
+                        false
+                    } else {
+                        action == MediaStore.ACTION_IMAGE_CAPTURE
+                    }
+                }
+                val selectedImageUri: Uri?
+                selectedImageUri = if (isCamera) {
+                    outputFileUri
+                } else {
+                    data?.data
+                }
+            }
+        }
     }
 
 }
