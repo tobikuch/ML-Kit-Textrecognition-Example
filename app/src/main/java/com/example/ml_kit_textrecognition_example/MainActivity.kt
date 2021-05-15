d47dcd2bae7ccaae221df952fd24c4cfbed5ab2a
Tobias Kuch <kuch@hm.edu>
2021-05-15 15:53:20 +0200

    check for permission

--- app/src/main/java/com/example/ml_kit_textrecognition_example/MainActivity.kt
+++ app/src/main/java/com/example/ml_kit_textrecognition_example/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.ml_kit_textrecognition_example
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.ml_kit_textrecognition_example.ui.main.MainFragment
 import timber.log.Timber
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +20,30 @@ class MainActivity : AppCompatActivity() {
 
         Timber.plant(Timber.DebugTree())
 
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+
+        if(checkPermission()){
+            Timber.i("Permission granted")
+        }else{
+            requestPermission()
+        }
+
     }
+
+    private fun checkPermission(): Boolean {
+        //TODO check also READ_EXTERNAL_STORAGE permission
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE),
+                200)
+    }
+
 }
